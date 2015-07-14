2f9222ee72e363cddaef156cd05a89aa600889d0
saeed khorsand <saeedkhorsand@rocketmail.com>
2015-07-14 21:33:37 +0430

    Run Methods In CSharp (C#) By Method Name
    Get Method Name and Args , Run Method And Return Values

---
+++ runMethodByName/Form1.Designer.cs
@@ -0,0 +1,72 @@
+﻿namespace runMethodByName
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnRun = new System.Windows.Forms.Button();
+            this.txtMethodName = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // btnRun
+            // 
+            this.btnRun.Location = new System.Drawing.Point(267, 12);
+            this.btnRun.Name = "btnRun";
+            this.btnRun.Size = new System.Drawing.Size(75, 23);
+            this.btnRun.TabIndex = 0;
+            this.btnRun.Text = "Run";
+            this.btnRun.UseVisualStyleBackColor = true;
+            this.btnRun.Click += new System.EventHandler(this.btnRun_Click);
+            // 
+            // txtMethodName
+            // 
+            this.txtMethodName.Location = new System.Drawing.Point(12, 12);
+            this.txtMethodName.Name = "txtMethodName";
+            this.txtMethodName.Size = new System.Drawing.Size(249, 20);
+            this.txtMethodName.TabIndex = 1;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(354, 262);
+            this.Controls.Add(this.txtMethodName);
+            this.Controls.Add(this.btnRun);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnRun;
+        private System.Windows.Forms.TextBox txtMethodName;
+    }
+}
+
---
+++ runMethodByName/Form1.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Windows.Forms;
+
+namespace runMethodByName
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnRun_Click(object sender, EventArgs e)
+        {
+            Invoker.CreateAndInvoke("runMethodByName.Tester", null, "TestMethod", null);
+
+            Invoker.CreateAndInvoke("runMethodByName.Tester", new[] { "constructorParam" }, "TestMethodUsingValueFromConstructorAndArgs", new object[] { "moo", false });
+        }
+    }
+
+    public static class Invoker
+    {
+        public static object CreateAndInvoke(string typeName, object[] constructorArgs, string methodName, object[] methodArgs)
+        {
+            Type type = Type.GetType(typeName);
+            object instance = Activator.CreateInstance(type, constructorArgs);
+
+            MethodInfo method = type.GetMethod(methodName);
+            return method.Invoke(instance, methodArgs);
+        }
+    }
+
+
+    public class Tester
+    {
+        public string _testField;
+
+        public Tester()
+        {
+        }
+
+        public Tester(string arg)
+        {
+            _testField = arg;
+        }
+
+        public void TestMethod()
+        {
+            MessageBox.Show("Called TestMethod");
+        }
+
+        public void TestMethodWithArg(string arg)
+        {
+            MessageBox.Show("Called TestMethodWithArg: " + arg);
+        }
+
+        public void TestMethodUsingValueFromConstructorAndArgs(string arg, bool arg2)
+        {
+            MessageBox.Show("Called TestMethodUsingValueFromConstructorAndArg " + arg + " " + arg2 + " " + _testField);
+        }
+
+        public string GetContstructorValue()
+        {
+            return _testField;
+        }
+    }
+}
